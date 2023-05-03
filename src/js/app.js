729b6ae77258974fcd16da5374d3179fb8eb6eef
Tetiana Nosenko <tanya2016english@gmail.com>
2023-05-03 18:51:32 +0200

    Init empty branch

---
+++ src/js/app.js
@@ -0,0 +1,100 @@
+import { settings, select, classNames } from './settings.js';
+import Product from './components/Product.js';
+import Cart from './components/Cart.js';
+import Booking from './components/Booking.js';
+import Home from './components/Home.js';
+
+const app = {
+  initBooking: function () {
+    const thisApp = this;
+    thisApp.booking = document.querySelector(select.containerOf.booking);
+    new Booking(thisApp.booking);
+  },
+  initPages: function () {
+    const thisApp = this;
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
+    thisApp.orderBookLinks = document.querySelectorAll(select.nav.orderBookLinks);
+
+    const idFromHash = window.location.hash.replace('#/', '');
+    let pageMatchingHash = thisApp.pages[0].id;
+    for (let page of thisApp.pages) {
+      if (page.id == idFromHash) {
+        pageMatchingHash = page.id;
+        break;
+      }
+    }
+    thisApp.activatePage(pageMatchingHash);
+
+
+    for (let link of [...thisApp.navLinks, ...thisApp.orderBookLinks]) {
+      link.addEventListener('click', thisApp.pageLinkClick);
+    }
+  },
+  pageLinkClick: function(event) {
+    const clickedElement = this;
+    event.preventDefault();
+    const id = clickedElement.getAttribute('href').replace('#', '');
+    app.activatePage(id);
+    window.location.hash = '#/' + id;
+  },
+  activatePage: function (pageId) {
+    const thisApp = this;
+    for (let page of thisApp.pages) {
+      page.classList.toggle(classNames.pages.active, page.id == pageId);
+    }
+    for (let link of thisApp.navLinks) {
+      link.classList.toggle(
+        classNames.nav.active,
+        link.getAttribute('href') == '#' + pageId
+      );
+    }
+  },
+
+  initMenu: function () {
+    const thisApp = this;
+    for (let productData in thisApp.data.products) {
+      new Product(thisApp.data.products[productData].id, thisApp.data.products[productData]);
+    }
+  },
+  initData: function () {
+    const thisApp = this;
+    thisApp.data = {};
+    const url = settings.db.url + '/' + settings.db.products;
+    fetch(url)
+      .then(function (rawResponse) {
+        return rawResponse.json();
+      })
+      .then(function (parsedResponse) {
+        thisApp.data.products = parsedResponse;
+        thisApp.initMenu();
+
+      });
+  },
+  initHome: function() {
+    const thisApp = this;
+    thisApp.home = new Home();
+  },
+  init: function () {
+    const thisApp = this;
+    thisApp.initHome();
+    thisApp.initPages();
+    thisApp.initData();
+    thisApp.initCart();
+    thisApp.initBooking();
+  },
+  initCart: function () {
+    const thisApp = this;
+    const cartElem = document.querySelector(select.containerOf.cart);
+    thisApp.cart = new Cart(cartElem);
+
+    thisApp.productList = document.querySelector(select.containerOf.menu);
+    thisApp.productList.addEventListener('add-to-cart', function (event) {
+      app.cart.add(event.detail.product);
+    });
+  },
+
+};
+app.init();
+
+
